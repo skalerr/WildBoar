e46d775d84b3901610959cae73d49374857f3a77
Skalerr <46483879+skalerr@users.noreply.github.com>
2023-03-05 17:38:38 +0500

    Исправил подгрузку сервисов

--- Domain/Entities/Product.cs
+++ Domain/Entities/Product.cs
@@ -4,10 +4,9 @@ namespace Domain.Entities;
 
 public class Product : ProductEntity
 {
+    public int Id { get; set; }
     public string Name { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-    public string Address { get; set; }
-    public string Phone { get; set; }
-    public string Role { get; set; }
+    public decimal Price { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
 }
--- WildBoar/Program.cs
+++ WildBoar/Program.cs
@@ -2,13 +2,27 @@ using DAL;
 using Autofac;
 using Autofac.Core;
 using System.ComponentModel;
+using Autofac;
+using Autofac.Core.Registration;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
+var containerBuilder = new ContainerBuilder();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.RegisterModule<DALModule>();
+containerBuilder.RegisterModule<DALModule>();
+var container = containerBuilder.Build();
+foreach (var registration in container.ComponentRegistry.Registrations)
+{
+    Console.WriteLine(registration.ToString());
+}
+
+
+builder.Services.AddSingleton(container);
 
 
 var app = builder.Build();
@@ -19,6 +33,15 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    var list = serviceProvider.GetServices<ComponentRegistration>();
+    foreach (var registration in list)
+    {
+        Console.WriteLine(registration.ToString());
+    }
 }
 
 app.UseHttpsRedirection();
